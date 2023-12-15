a5a92e19a4d10fa9390cdc3cf90b0ab45ebac792
Joana Maria <joanamariacardoso1601@gmail.com>
2023-12-15 22:35:34 +0000

    Capturar o nome do utilizador no input e armazenar no local storage #4

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
-const renderPage = async (pageName: string): Promise<void> => {
-  const html = await fetch(`pages/${pageName}.html`).then((resp) =>
-    resp.text()
-  );
+const renderPage = (pageName: string): void => {
+  fetch(`pages/${pageName}.html`)
+    .then((resp) => resp.text())
+    .then((html) => {
+      const rootElem = document.getElementById("root") as HTMLElement;
+      rootElem.innerHTML = html;
 
-  const rootElem = document.getElementById("root") as HTMLElement;
-  rootElem.innerHTML = html;
+      const form = document.getElementById("root") as HTMLFormElement;
+
+      form.addEventListener("click", function () {
+        const inputElem = document.getElementById(
+          "input-name"
+        ) as HTMLInputElement;
+        localStorage.setItem("name", inputElem.value);
+
+        fetch(`pages/quiz.html`)
+          .then((resp) => resp.text())
+          .then((html) => {
+            rootElem.innerHTML = html;
+          });
+      });
+    });
 };
+
+renderPage("start");
